d6a5f31eddf73c326f2cce86004cd88b7a8e6196
Rafael Bogo <rafaelbogo52@gmail.com>
2025-05-02 15:49:31 -0300

    Equipamento concluido

--- cypress/e2e/locacao-equipamento.cy.js
+++ cypress/e2e/locacao-equipamento.cy.js
@@ -9,7 +9,7 @@ Feature: Cadastro de Locação - Modulo 2 (Equipamentos)
 Scenario: Adicionar equipamento a locação com sucesso
   Given o usuário esta logado no sistema ADM Leve
   When preenche os campos de um novo equipamento
-  Then o equipamento é adicionado a lista da locação
+  Then o equipamento é adicionado  lista da locação
 */
 
 describe('Locação - Adicionar equipamento', () => {
@@ -27,9 +27,11 @@ describe('Locação - Adicionar equipamento', () => {
       termoBusca: '1',
       quantidade: '300',
       dias: '1000',
-      complemento: 'Locação teste'
+      complemento: 'Locação teste',
+      dataInicial: "28042025",
+      horaInicial: "22:00",
+      dataFinal: "10052025",
+      horaFinal: "20:00",
     })
-
-    locacaoForm.validarEquipamentoAdicionado()
   })
 })
--- cypress/support/locacaoForm.js
+++ cypress/support/locacaoForm.js
@@ -1,22 +1,26 @@
 export class LocacaoForm {
     elements = {
       descricao: () => cy.get('input#descricao'),
-    data: () => cy.get('input#data'),
-    diasDuracao: () => cy.get('input#diasDuracao'),
-    observacoes: () => cy.get('textarea#observacoes'),
-    nomeCliente: () => cy.get('#nomeCliente'),
-    lupaCliente: () => cy.get('button[data-target="#pesquisaRapidaClientes"]'),
-    clienteModal: () => cy.get('input#nomeClienteModal'),
-    botaoPesquisarCliente: () => cy.get('button.js-pesquisa-rapida-clientes-btn'),
-    resultadoCliente: (nome) => cy.contains('td', nome),
-    equipamentoInput: () => cy.get('input.form-control.equipamentoAutoComplete'),
-    equipamentoSelecionado: () => cy.get('div.equipamento-autocomplete'),
-    quantidade: () => cy.get('input#quantidade'),
-    diasEquipamento: () => cy.get('input#qtdDias'),
-    descricaoComplementar: () => cy.get('input#descrComplementar'),
-    botaoAdicionarEquipamento: () => cy.get('button#adicionarEquipamento'),
-    listaEquipamento: () => cy.get('table tbody tr'),
-    totalItens: () => cy.get('span').contains('Itens:'),
+      data: () => cy.get('input#data'),
+      diasDuracao: () => cy.get('input#diasDuracao'),
+      observacoes: () => cy.get('textarea#observacoes'),
+      nomeCliente: () => cy.get('#nomeCliente'),
+      lupaCliente: () => cy.get('button[data-target="#pesquisaRapidaClientes"]'),
+      clienteModal: () => cy.get('input#nomeClienteModal'),
+      botaoPesquisarCliente: () => cy.get('button.js-pesquisa-rapida-clientes-btn'),
+      resultadoCliente: (nome) => cy.contains('td', nome),
+      equipamentoInput: () => cy.get('input.form-control.equipamentoAutoComplete'),
+      equipamentoSelecionado: () => cy.get('div.equipamento-autocomplete'),
+      quantidade: () => cy.get('input#quantidade'),
+      diasEquipamento: () => cy.get('input#qtdDias'),
+      descricaoComplementar: () => cy.get('input#descrComplementar'),
+      botaoAdicionarEquipamento: () => cy.get('button#adicionaEquipamento'),
+      listaEquipamento: () => cy.get('table tbody tr'),
+      totalItens: () => cy.get('span').contains('Itens:'),
+      dataInicial: () => cy.get('input#dataInicial'),
+      horaInicial: () => cy.get("input#horaInicial"),
+      dataFinal: () => cy.get("input#dataDevolucao"),
+      horaFinal: () => cy.get("input#horaDevolucao1"),
     }
   
     preencherDescricao(texto) {
@@ -45,18 +49,21 @@ export class LocacaoForm {
       this.elements.resultadoCliente(nome).click();
     }
 
-    adicionarEquipamento({ termoBusca, quantidade, dias, complemento }) {
+    adicionarEquipamento({ termoBusca, quantidade, dias, complemento, dataInicial, horaInicial, dataFinal, horaFinal }) {
       this.elements.equipamentoInput().type(termoBusca);
-      cy.wait(500);
+      cy.wait(300);
       this.elements.equipamentoSelecionado().click();
       this.elements.quantidade().clear().type(quantidade);
       this.elements.diasEquipamento().clear().type(dias);
       this.elements.descricaoComplementar().clear().type(complemento);
+      this.elements.descricaoComplementar().clear().type(complemento);
+      this.elements.dataInicial().clear().type(dataInicial);
+      this.elements.horaInicial().clear().type(horaInicial);
+      this.elements.dataFinal().clear().type(dataFinal);
+      this.elements.horaFinal().clear().type(horaFinal);
       this.elements.botaoAdicionarEquipamento().click();
+
+      cy.get('.jq-toast-single').should('be.visible').and('contain.text', 'Equipamento adicionado.')
     }
 
-    validarEquipamentoAdicionado() {
-      this.elements.listaEquipamento().should('have.length.greaterThan', 0);
-      this.elements.totalItens().should('contain.text', '1');
-    }
 }
